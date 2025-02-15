146ee02621dfec3a5430de1ac8afa1f855b69c5b
AdelleStuuu <adelledoesstuff@gmail.com>
2025-02-15 20:47:36 +0800

    move back

--- webs/public/js/script.js
+++ webs/public/js/script.js
@@ -15,27 +15,3 @@ if (signUpButton && signInButton) {
     });
 }
 
-async function uploadFile() {
-    const fileInput = document.getElementById("fileInput");
-    const file = fileInput.files[0];
-    if (!file) {
-        alert("Please select a file!");
-        return;
-    }
-
-    const formData = new FormData();
-    formData.append("file", file);
-
-    try {
-        const response = await fetch("http://localhost:5000/upload", {
-            method: "POST",
-            body: formData
-        });
-
-        const result = await response.json();
-        alert("File uploaded successfully!");
-        console.log(result);
-    } catch (error) {
-        console.error("Error uploading file:", error);
-    }
-}
