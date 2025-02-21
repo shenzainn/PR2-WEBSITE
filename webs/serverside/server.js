e6796c85e347f7b5286afeee0e75d8dccaadc307
AdelleStuuu <adelledoesstuff@gmail.com>
2025-02-21 14:19:52 +0800

    removed deprecated part 2

--- webs/serverside/server.js
+++ webs/serverside/server.js
@@ -9,11 +9,10 @@ require("dotenv").config();
 const app = express();
 
 // MongoDB Connection
-const mongoURI = "mongodb://localhost:27017/mydatabase";
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
+mongoose.connect(mongoURI)
+  .then(() => console.log('MongoDB connected.'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 const conn = mongoose.connection;
 let gfs, bucket;
