7158ab35f6b1e6bfc596c1b16298457053175c5c
youngblake <roadkillah26@gmail.com>
2025-02-23 02:51:44 +0800

    fix script.js T_T

--- webs/public/js/script.js
+++ webs/public/js/script.js
@@ -1,3 +1,25 @@
+const signUpButton = document.getElementById('signUpButton'); 
+const signInButton = document.getElementById('signInButton'); 
+const signInForm = document.getElementById('signIn'); 
+const signUpForm = document.getElementById('signUp'); 
+
+if (signUpButton && signInButton) {
+    signUpButton.addEventListener('click', function () {
+        signInForm.style.display = "none";
+        signUpForm.style.display = "block";
+    });
+
+    signInButton.addEventListener('click', function () {
+        signInForm.style.display = "block";
+        signUpForm.style.display = "none";
+    });
+}
+
+// Toggle Sidebar Menu
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.toggle('active');
+});
+
 document.getElementById('loginForm').addEventListener('submit', async function(event) {
     event.preventDefault(); // Prevent form from reloading the page
 
