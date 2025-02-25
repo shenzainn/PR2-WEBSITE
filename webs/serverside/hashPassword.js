3a5c47c48ad067c3ba0684e3c7b40b0a7b9f23e0
youngblake <roadkillah26@gmail.com>
2025-02-25 23:53:54 +0800

    added hashPassword Generator

---
+++ webs/serverside/hashPassword.js
@@ -0,0 +1,11 @@
+const bcrypt = require("bcryptjs");
+
+const password = "mypassword123"; // Change this to your desired password
+
+bcrypt.hash(password, 10, (err, hash) => {
+    if (err) {
+        console.error("Error hashing password:", err);
+    } else {
+        console.log("Hashed password:", hash);
+    }
+});
